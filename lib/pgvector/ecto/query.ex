c322fe1e23b4db6407735480e3e65822af465354
Malte Rohde <malte.rohde@posteo.de>
2023-01-25 22:04:35 +0100

    Allow composition of fragment macros (#3)

--- lib/pgvector/ecto/query.ex
+++ lib/pgvector/ecto/query.ex
@@ -2,19 +2,19 @@ if Code.ensure_loaded?(Ecto) do
   defmodule Pgvector.Ecto.Query do
     defmacro l2_distance(column, value) do
       quote do
-        fragment("? <-> ?::vector", unquote(column), unquote(value))
+        fragment("(? <-> ?::vector)", unquote(column), unquote(value))
       end
     end
 
     defmacro max_inner_product(column, value) do
       quote do
-        fragment("? <#> ?::vector", unquote(column), unquote(value))
+        fragment("(? <#> ?::vector)", unquote(column), unquote(value))
       end
     end
 
     defmacro cosine_distance(column, value) do
       quote do
-        fragment("? <=> ?::vector", unquote(column), unquote(value))
+        fragment("(? <=> ?::vector)", unquote(column), unquote(value))
       end
     end
   end
