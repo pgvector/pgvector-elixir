c14f1fc22fcd47e43adb97d62ff675c0d6d41fae
José Valim <jose.valim@gmail.com>
2023-03-24 04:58:53 +0100

    Don't check if Postgrex is loaded (#4)
    It is a required dependency, so it is always loaded.

--- lib/pgvector/extensions/vector.ex
+++ lib/pgvector/extensions/vector.ex
@@ -1,36 +1,34 @@
-if Code.ensure_loaded?(Postgrex) do
-  defmodule Pgvector.Extensions.Vector do
-    import Postgrex.BinaryUtils, warn: false
+defmodule Pgvector.Extensions.Vector do
+  import Postgrex.BinaryUtils, warn: false
 
-    def init(opts), do: Keyword.get(opts, :decode_binary, :copy)
+  def init(opts), do: Keyword.get(opts, :decode_binary, :copy)
 
-    def matching(_), do: [type: "vector"]
+  def matching(_), do: [type: "vector"]
 
-    def format(_), do: :binary
+  def format(_), do: :binary
 
-    def encode(_) do
-      quote do
-        vec when is_list(vec) ->
-          data = unquote(__MODULE__).encode_vector(vec)
-          [<<IO.iodata_length(data)::int32()>> | data]
-      end
+  def encode(_) do
+    quote do
+      vec when is_list(vec) ->
+        data = unquote(__MODULE__).encode_vector(vec)
+        [<<IO.iodata_length(data)::int32()>> | data]
     end
+  end
 
-    def decode(_) do
-      quote do
-        <<len::int32(), data::binary-size(len)>> ->
-          unquote(__MODULE__).decode_vector(data)
-      end
+  def decode(_) do
+    quote do
+      <<len::int32(), data::binary-size(len)>> ->
+        unquote(__MODULE__).decode_vector(data)
     end
+  end
 
-    def encode_vector(vec) do
-      dim = length(vec)
-      bin = Enum.map(vec, fn v -> <<v::float32>> end)
-      [<<dim::uint16>>, <<0::uint16>> | bin]
-    end
+  def encode_vector(vec) do
+    dim = length(vec)
+    bin = Enum.map(vec, fn v -> <<v::float32>> end)
+    [<<dim::uint16>>, <<0::uint16>> | bin]
+  end
 
-    def decode_vector(<<dim::uint16, 0::uint16, bin::binary-size(dim)-unit(32)>>) do
-      for <<v::float32 <- bin>>, do: v
-    end
+  def decode_vector(<<dim::uint16, 0::uint16, bin::binary-size(dim)-unit(32)>>) do
+    for <<v::float32 <- bin>>, do: v
   end
 end
