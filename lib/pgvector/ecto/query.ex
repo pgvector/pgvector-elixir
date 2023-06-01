0cc153c12ae330b3c77d03a2bcc027af5b89a264
Andrew Kane <andrew@ankane.org>
2023-05-31 21:34:19 -0700

    Added docs for Pgvector.Ecto.Query [skip ci]

--- lib/pgvector/ecto/query.ex
+++ lib/pgvector/ecto/query.ex
@@ -1,17 +1,30 @@
 if Code.ensure_loaded?(Ecto) do
   defmodule Pgvector.Ecto.Query do
+    @moduledoc """
+    Distance functions for Ecto
+    """
+
+    @doc """
+    Returns the L2 distance
+    """
     defmacro l2_distance(column, value) do
       quote do
         fragment("(? <-> ?::vector)", unquote(column), unquote(value))
       end
     end
 
+    @doc """
+    Returns the negative inner product
+    """
     defmacro max_inner_product(column, value) do
       quote do
         fragment("(? <#> ?::vector)", unquote(column), unquote(value))
       end
     end
 
+    @doc """
+    Returns the cosine distance
+    """
     defmacro cosine_distance(column, value) do
       quote do
         fragment("(? <=> ?::vector)", unquote(column), unquote(value))
