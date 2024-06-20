34a4d71802e52389c7033ec292bd9ce1528d1066
Andrew Kane <andrew@ankane.org>
2024-06-20 00:10:18 -0700

    Improved SparseVector tests

--- test/sparse_vector_test.exs
+++ test/sparse_vector_test.exs
@@ -2,17 +2,17 @@ defmodule SparseVectorTest do
   use ExUnit.Case
 
   test "sparse vector" do
-    vector = Pgvector.SparseVector.new([1, 2, 3])
+    vector = Pgvector.SparseVector.new([1, 0, 2, 0, 3, 0])
     assert vector == vector |> Pgvector.SparseVector.new()
   end
 
   test "list" do
-    list = [1.0, 2.0, 3.0]
+    list = [1.0, 0.0, 2.0, 0.0, 3.0, 0.0]
     assert list == list |> Pgvector.SparseVector.new() |> Pgvector.to_list()
   end
 
   test "tensor" do
-    tensor = Nx.tensor([1.0, 2.0, 3.0], type: :f32)
+    tensor = Nx.tensor([1.0, 0.0, 2.0, 0.0, 3.0, 0.0], type: :f32)
     assert tensor == tensor |> Pgvector.SparseVector.new() |> Pgvector.to_tensor()
   end
 
@@ -22,23 +22,23 @@ defmodule SparseVectorTest do
   end
 
   test "dimensions" do
-    vector = Pgvector.SparseVector.new([1, 2, 3])
-    assert 3 == vector |> Pgvector.SparseVector.dimensions()
+    vector = Pgvector.SparseVector.new([1, 0, 2, 0, 3, 0])
+    assert 6 == vector |> Pgvector.SparseVector.dimensions()
   end
 
   test "indices" do
-    vector = Pgvector.SparseVector.new([1, 2, 3])
-    assert [0, 1, 2] == vector |> Pgvector.SparseVector.indices()
+    vector = Pgvector.SparseVector.new([1, 0, 2, 0, 3, 0])
+    assert [0, 2, 4] == vector |> Pgvector.SparseVector.indices()
   end
 
   test "values" do
-    vector = Pgvector.SparseVector.new([1, 2, 3])
+    vector = Pgvector.SparseVector.new([1, 0, 2, 0, 3, 0])
     assert [1, 2, 3] == vector |> Pgvector.SparseVector.values()
   end
 
   test "inspect" do
-    vector = Pgvector.SparseVector.new([1, 2, 3])
-    assert "Pgvector.SparseVector.new(%{0 => 1.0, 1 => 2.0, 2 => 3.0}, 3)" == inspect(vector)
+    vector = Pgvector.SparseVector.new([1, 0, 2, 0, 3, 0])
+    assert "Pgvector.SparseVector.new(%{0 => 1.0, 2 => 2.0, 4 => 3.0}, 6)" == inspect(vector)
   end
 
   test "equals" do
